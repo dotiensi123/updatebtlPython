f364db686109126c64ff5f05d4047981c93023a5
dotiensi123 <taitiensi57@gmail.com>
2023-04-14 13:38:06 +0700

    version 2

--- project2/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from .import views
-app_name = 'User'
-urlpatterns = [
-    path('register/',views.registerUser.as_view(), name = 'register'),
-    path('login/',views.loginUser.as_view(), name = 'login'),
-    path('logout/',views.logoutUser, name = 'logout'),
-]
